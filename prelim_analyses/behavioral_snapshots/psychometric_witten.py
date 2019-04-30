89f2da50e22f20d25d44f853c5797e0ee0678372
ibladmin <alejandropan@gmail.com>
2019-04-29 22:43:37 -0400

    per mouse analysis

--- prelim_analyses/behavioral_snapshots/psychometric_witten.py
+++ prelim_analyses/behavioral_snapshots/psychometric_witten.py
@@ -16,6 +16,7 @@ from ibl_pipeline import reference, subject, action, acquisition, data, behavior
 from ibl_pipeline.analyses import behavior as behavior_analysis
 from load_mouse_data_datajoint import *  # this has all plotting functions
 from ibl_pipeline.utils import psychofit as psy
+import seaborn as sns
 
 
 #TODO add restriction for untrainable #2,793,497 trials #89,442 from witten trained
@@ -68,3 +69,75 @@ ax.plot(unique_signed_contrasts * 100, prop_right_trials)
 ax.plot(x, y)
 ax.set_xlabel('Signed Contrast (%)')
 ax.set_ylabel('P(right choice)')
+
+
+#By mouse in lab
+
+def psy_by_mouse_local (unique_signed_contrasts,labname):
+        
+        labname  = labname
+        mice = pd.DataFrame.from_dict((subject.Subject()) 
+            * (subject.SubjectLab() & 'lab_name="{}"'.format(labname)) 
+            * (behavior_analysis.SessionTrainingStatus() & 'training_status="trained"  '))     
+        
+        psy_df = pd.DataFrame(columns = [unique_signed_contrasts])
+        
+        for row , mouse in enumerate(mice.subject_nickname.unique()):
+            
+            key = ((subject.Subject() & 'subject_nickname = "{}"'.format(mouse)) 
+            * (behavior.TrialSet() & 'n_trials > 600') * (subject.SubjectLab() & 'lab_name="{}"'.format(labname)) 
+            * (behavior_analysis.SessionTrainingStatus() & 'training_status="trained"  ')).fetch('KEY')
+            
+            choice, cont_left, cont_right = (behavior.TrialSet.Trial & key).fetch('trial_response_choice', 
+                                                                                  'trial_stim_contrast_left', 
+                                                                                  'trial_stim_contrast_right')
+            signed_contrasts = cont_right - cont_left
+            right_choices = choice == 'CCW'
+            
+            total_trials = []
+            right_trials = []
+            
+            for cont in unique_signed_contrasts:
+                matching = (signed_contrasts == cont)
+                total_trials.append(np.sum(matching))
+                right_trials.append(np.sum(right_choices[matching]))
+            
+            prop_right_trials = np.divide(right_trials, total_trials)
+            psy_df.loc[row,:] = prop_right_trials
+            
+        return psy_df
+        
+#By mouse total
+def psy_by_mouse (unique_signed_contrasts):
+   
+    
+      mice = pd.DataFrame.from_dict((subject.Subject()) 
+            * (subject.SubjectLab()) 
+            * (behavior_analysis.SessionTrainingStatus() & 'training_status="trained"  '))     
+        
+      psy_df = pd.DataFrame(columns = [unique_signed_contrasts])
+      
+      for row , mouse in enumerate(mice.subject_nickname.unique()):
+            
+            key = ((subject.Subject() & 'subject_nickname = "{}"'.format(mouse)) 
+            * (behavior.TrialSet() & 'n_trials > 100') * (subject.SubjectLab()) 
+            * (behavior_analysis.SessionTrainingStatus() & 'training_status="trained"  ')).fetch('KEY')
+            
+            choice, cont_left, cont_right = (behavior.TrialSet.Trial & key).fetch('trial_response_choice', 
+                                                                                  'trial_stim_contrast_left', 
+                                                                                  'trial_stim_contrast_right')
+            signed_contrasts = cont_right - cont_left
+            right_choices = choice == 'CCW'
+            
+            total_trials = []
+            right_trials = []
+            
+            for cont in unique_signed_contrasts:
+                matching = (signed_contrasts == cont)
+                total_trials.append(np.sum(matching))
+                right_trials.append(np.sum(right_choices[matching]))
+            
+            prop_right_trials = np.divide(right_trials, total_trials)
+            psy_df.loc[row,:] = prop_right_trials
+        
+      return psy_df

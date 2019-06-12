103079eb3276cfc3dffc9a8f45b0ce6b2bbd651d
ibladmin <alejandropan@gmail.com>
2019-06-12 01:55:35 -0400

    final glm

---
+++ prelim_analyses/behavioral_snapshots/glm.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 16:55:17 2019
+
+
+Predictors:  Rewarded choice and unrewarded choice
+These two predictors include  correct choice and reward i.e
+
+R (Right_Choice = True, Correct_choice = True, Reward = True) = 1
+R (Right_Choice = True, Correct_choice = False, Reward = True) = 1 - This never happens
+U(Right_Choice = True, Correct_choice = False, Reward = False) = 0
+U(Right_Choice = True, Correct_choice = True, Reward = False) = 0
+
+R (Right_Choice = False, Correct_choice = True, Reward = True) =  -1
+R (Right_Choice = False, Correct_choice = False, Reward = True) =  -1 - This never happens
+U(Right_Choice = False, Correct_choice = False, Reward = False) = 0 /
+U(Right_Choice = False, Correct_choice = True, Reward = False) =  0 /
+
+
+TODO:  invert choice vector so that negative value correspond to negative contrast in correct trials?
+
+@author: ibladmin
+
+TODO:  Description of psy_df
+
+"""
+
+import statsmodels.api as sm
+import seaborn as sns; sns.set()
+import numpy as np
+import pandas as pd
+import time
+import statsmodels.genmod.bayes_mixed_glm as  bayes
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+def glm_reward(psy_df,  mixed_effects=False, sex =  True):
+from sklearn.model_selection import KFold
+
+ 
+def  glm_logit(psy_df):
+
+   ##calculate useful variables
+    
+    #Calculate signed contrast
+    if not 'signed_contrasts' in psy_df:
+        psy_df.loc[:,'contrastRight'] = psy_df['contrastRight'].fillna(0)
+        psy_df.loc[:,'contrastLeft']  = psy_df['contrastLeft'].fillna(0)
+        psy_df.loc[:,'signed_contrasts'] =  (psy_df['contrastRight'] - psy_df['contrastLeft'])
+
+        
+    #Add sex if not present
+    if not 'sex' in psy_df:
+        psy_df.loc[:,'sex'] = np.empty([psy_df.shape[0],1])
+        mice  = sorted(psy_df['mouse_name'].unique())
+        for mouse in mice:
+            sex = input('Sex of animal ')
+            psy_df.loc[ psy_df['mouse_name'] == mouse, ['sex']]  = sex
+    
+    #make separate datafrme 
+    data =  psy_df.loc[ :, ['sex', 'mouse_name', 'feedbackType', 'signed_contrasts', 'choice','ses']]
+    
+    ## Build predictor matrix
+    
+    #Rewardeded choices: 
+    data.loc[(data['choice'] == -1) & (data['feedbackType'] == -2) , 'rchoice']  = 0
+    data.loc[(data['choice'] == -1) & (data['feedbackType'] == -1) , 'rchoice']  = 0
+    data.loc[(data['choice'] == -1) & (data['feedbackType'] == 1) , 'rchoice']  = -1
+    data.loc[(data['choice'] == 1) & (data['feedbackType'] == 1) , 'rchoice']  = 1
+    data.loc[(data['choice'] == 1) & (data['feedbackType'] == -2) , 'rchoice']  = 0
+    data.loc[(data['choice'] == 1) & (data['feedbackType'] == -1) , 'rchoice']  = 0
+    
+    #Unrewarded choices: 
+    data.loc[(data['choice'] == -1) & (data['feedbackType'] == -2) , 'uchoice']  = -1
+    data.loc[(data['choice'] == -1) & (data['feedbackType'] == -1) , 'uchoice']  = -1
+    data.loc[(data['choice'] == -1) & (data['feedbackType'] == 1) , 'uchoice']  = 0
+    data.loc[(data['choice'] == 1) & (data['feedbackType'] == 1) , 'uchoice']  = 0
+    data.loc[(data['choice'] == 1) & (data['feedbackType'] == -2) , 'uchoice']  = 1
+    data.loc[(data['choice'] == 1) & (data['feedbackType'] == -1) , 'uchoice']  = 1
+    
+    #Drop nogos
+    data = data.drop(data.index[data['feedbackType'] == 0],axis=0)
+    ## Change -1 for 0 in choice 
+    data.loc[(data['choice'] == -1), 'choice'] = 0
+    
+    #make Revidence and LEvidence
+    data.loc[(data['signed_contrasts'] >= 0), 'Revidence'] = data.loc[(data['signed_contrasts'] >= 0), 'signed_contrasts'].abs()
+    data.loc[(data['signed_contrasts'] <= 0), 'Revidence'] = 0
+    data.loc[(data['signed_contrasts'] <= 0), 'Levidence'] = data.loc[(data['signed_contrasts'] <= 0), 'signed_contrasts'].abs()
+    data.loc[(data['signed_contrasts'] >= 0), 'Levidence'] = 0
+    
+    
+    #previous choices and evidence
+    
+    no_tback = 5 #no of trials back
+    
+    start = time.time()
+    for date in sorted(data['ses'].unique()):
+        for i in range(no_tback):
+            data.loc[data['ses'] == date,'rchoice%s' %str(i+1)] =  data.loc[data['ses'] == date,'rchoice'].shift(i+1) #no point in 0 shift
+            data.loc[data['ses'] == date,'uchoice%s' %str(i+1)] =  data.loc[data['ses'] == date,'uchoice'].shift(i+1) #no point in 0 shift
+            data.loc[data['ses'] == date,'Levidence%s' %str(i+1)] =  data.loc[data['ses'] == date,'Levidence'].shift(i+1) #no point in 0 shift
+            data.loc[data['ses'] == date,'Revidence%s' %str(i+1)] =  data.loc[data['ses'] == date,'Revidence'].shift(i+1) #no point in 0 shift
+    end = time.time()
+    print(end - start)
+    
+    
+    #Remove first 5 trials from each ses
+    #for date in sorted(data['ses'].unique()):
+    #    data  = data.drop(data.index[data['ses'] == date][0:5] ,axis=0)
+        
+    #data = data.reset_index()
+    #Drop unnecessary elements
+    #data =  data.drop(columns  = ['feedbackType','sex', 'ses','signed_contrasts', 'index'])
+    
+    data =  data.dropna()
+    
+    mdata = data.loc[(data['sex'] == 'F')]
+    fdata = data1.loc[(data1['sex'] == 'M')]
+    ## construct our model, with contrast as a variable
+    
+    ##Bayeasian mixed effects #need to change ident and exog_VC to account for mixed effects
+    result, r2  =  load_regression(data)
+    mresult, mr2 = load_regression(mdata)
+    fresult, fr2 = load_regression(fdata)
+    
+    return mresult, fresult, mr2, fr2
+
+def load_regression (data, mixed_effects  = False):
+    
+    endog  = pd.DataFrame(data['choice'])
+    exog  = data[[ 'Revidence', 'Levidence',\
+            'rchoice1', 'uchoice1', 'Levidence1',\
+                  'Revidence1', 'rchoice2', 'uchoice2', 'Levidence2', 'Revidence2',\
+                  'rchoice3', 'uchoice3', 'Levidence3', 'Revidence3', 'rchoice4',\
+                  'uchoice4', 'Levidence4', 'Revidence4', 'rchoice5', 'uchoice5',\
+                  'Levidence5', 'Revidence5']]
+    
+    cols = list(exog.columns)
+    
+    #Normalising predictors
+    for col in cols:
+        col_zscore = col + '_zscore'
+        exog[col_zscore] = (exog[col] - exog[col].mean())/exog[col].std(ddof=0)
+    
+    exog  =exog.drop(columns = [ 'Revidence', 'Levidence',\
+            'rchoice1', 'uchoice1', 'Levidence1',\
+                      'Revidence1', 'rchoice2', 'uchoice2', 'Levidence2', 'Revidence2',\
+                      'rchoice3', 'uchoice3', 'Levidence3', 'Revidence3', 'rchoice4',\
+                      'uchoice4', 'Levidence4', 'Revidence4', 'rchoice5', 'uchoice5',\
+                      'Levidence5', 'Revidence5'] )
+        
+    
+    ##Cross validation
+    
+    if mixed_effects == False :
+
+    ##Frequentist wihtout mixed effects
+        X_train, X_test, y_train, y_test = train_test_split(exog, np.ravel(endog), test_size=0.3)
+        logit_model = sm.Logit(y_train,X_train)
+        result=logit_model.fit()
+        print(result.summary2())
+
+        #crossvalidation
+        
+        
+        cv = KFold(n_splits=10,  shuffle=False)
+        logreg1 = LogisticRegression()
+        r2  = cross_val_score(logreg1, exog, endog, cv=10)
+        print( 'Accuracy  = ' , r2.mean())
+        print(end - start)
+
+       
+    #cross validate  with sklearn 
+    #NOTE: Currently cross validating without mixed effects
+    
+        logreg = LogisticRegression()
+        logreg.fit(X_train, y_train)
+        accu = logreg.score(X_test, y_test)
+        print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+
+    return result, r2
+
+
+""" TODO nmiced effects GLM
+
+     X_train = X_train.drop(columns = 'mouse_name')
+    X_test = X_test.drop(columns = 'mouse_name')
+    y_train = pd.DataFrame(y_train)
+    y_test = pd.DataFrame(y_test)
+    Mixed effects models no done yet
+    y_train = pd.DataFrame(y_train)
+    exog_vc = X_train
+    for i, name in enumerate(sorted(X_train['mouse_name'].unique())):
+        exog_vc.loc[(exog_vc['mouse_name'] == name)] = i
+    ident =  np.ones(exog_vc.shape[1], dtype=np.int)
+    X_train = X_train.drop(columns = 'mouse_name')
+    X_test = X_test.drop(columns = 'mouse_name')
+    exog_vc  = exog_vc.drop(columns = 'mouse_name')
+    exog_vc = pd.DataFrame(exog_vc.iloc[:,1])
+    exog_vc  = exog_vc + 1
+    ident  = np.ones([1])  
+    model1 = bayes.BinomialBayesMixedGLM(y_train, X_train, exog_vc,ident)
+    result = model1.fit_map()
+    result.summary()
+    """"
+    
+
+#####
+#To do model psychometric
+####
+
+
+
+    
+    
--- prelim_analyses/behavioral_snapshots/ibl_glm.py
+++ prelim_analyses/behavioral_snapshots/ibl_glm.py
@@ -8,16 +8,15 @@ prepare ibl trial dataframe for IBL
 """
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from scipy import stats
 ## CONNECT TO datajoint
 import datajoint as dj
 dj.config['database.host'] = 'datajoint.internationalbrainlab.org'
 from ibl_pipeline.analyses import behavior as behavior_analysis
-from ibl_pipeline import reference, subject, action, acquisition, data, behavior
+from ibl_pipeline import reference, subject, behavior
 from load_mouse_data_datajoint import *  # this has all plotting functions
-
+import seaborn as sns
+from glm import *
 
 
 
@@ -40,4 +39,26 @@ trials_ibl.loc[(trials_ibl['choice']=='No Go'), 'choice'] = 0
 
 #Remove 0.5 block?
 
-psy_df =  trials_ibl
+
+psy_df =  trials_ibl.loc[(trials_ibl['trial_stim_prob_left'] == 0.8) | (trials_ibl['trial_stim_prob_left'] == 0.2)]
+
+mresult, fresult, mr2, fr2  = glm_logit(psy_df)
+
+mresults  =  pd.DataFrame({"Predictors": mresult.model.exog_names , "Coef" : mresult.params.values,\
+                          "SEM": mresult.bse.values, "Sex": "M"})
+mresults = mresults.reindex(mresults.Coef.abs().sort_values(ascending=False).index)
+fresults  =  pd.DataFrame({"Predictors": fresult.model.exog_names , "Coef" : fresult.params.values,\
+                          "SEM": mresult.bse.values, "Sex": "F"}).reindex(mresults.Coef.abs().sort_values().index)
+fresults = fresults.reindex(fresults.Coef.abs().sort_values(ascending=False).index)
+results  = pd.concat([mresults, fresults]) 
+
+
+#Plotting
+fig, ax = plt.subplots(figsize=(12, 9))
+ax  = sns.barplot(x = 'Predictors', y = 'Coef', data=results, hue='Sex')    
+ax.set_xticklabels( results['Predictors'], rotation=-90)
+ax.set_ylabel('coef')
+ax.axhline(y=0, linestyle='--', color='black', linewidth=2)
+fig.suptitle ('GLM Biased Blocks')
+fig.savefig("glm_sex_diff.pdf")
+
